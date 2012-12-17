9a041d517595663ae4f2ca8e7a00717d995090d8
Jan Alexander Stormark Bremnes <janbremnes@gmail.com>
2012-12-17 23:24:01 +0100

    added script to build database, some mongodb tips and a perl script for testing regex

--- build_mongo_db.py
+++ build_mongo_db.py
@@ -7,14 +7,10 @@ coll = connection.test_database.courses
 
 path = 'fag/'
 listing = os.listdir(path)
-i = 0
 for infile in listing:
     print infile
     current_file = open(path+infile, 'r').readline()
     course = json.loads(current_file)
     coll.insert(course)
-    i += 1
-    if i % 300 == 0:
-        print '.'    
 
 
---
+++ test.pl
@@ -0,0 +1,9 @@
+
+$a = "Emne bladidi dTDT4117 greret"; 
+
+if( $a =~ m/\D{3}\d{4}/ ){
+	print "hei"; 
+ }else {
+	print "nope";
+ }
+
