9b9843543565f80f135301b371625a4016475f96
Jan Alexander Stormark Bremnes <janbremnes@gmail.com>
2015-10-28 01:30:46 +0100

    updated the mongodb tips to new mongo version

--- build_mongo_db.py
+++ build_mongo_db.py
@@ -2,15 +2,15 @@ import pymongo
 import json
 import os
 
-connection = pymongo.Connection()
-coll = connection.test_database.courses
+client = pymongo.MongoClient()
+db = client.test_database.courses
 
 path = 'fag/'
 listing = os.listdir(path)
 for infile in listing:
-    print infile
+    print (infile)
     current_file = open(path+infile, 'r').readline()
     course = json.loads(current_file)
-    coll.insert(course)
+    db.insert_one(course)
 
 
